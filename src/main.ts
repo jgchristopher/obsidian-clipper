5cceb2456dfdf8aebc981c1f4394673b968211ed
John Christopher <jgchristopher@gmail.com>
2023-02-15 20:49:26 -0600

    save settings on setting change

--- src/main.ts
+++ src/main.ts
@@ -11,107 +11,117 @@ import { writable } from "svelte/store";
 import type { SvelteComponent } from "svelte";
 
 export default class ObsidianClipperPlugin extends Plugin {
-	settings: ObsidianClipperSettings;
+  settings: ObsidianClipperSettings;
 
-	async onload() {
-		await this.loadSettings();
-		this.addSettingTab(new SettingTab(this.app, this));
+  async onload() {
+    await this.loadSettings();
+    this.addSettingTab(new SettingTab(this.app, this));
 
-		this.addCommand({
-			id: "copy-bookmarklet-address",
-			name: "copy the Obsidian Clipper bookmarklet for this vault",
-			callback: () => this.handleCopyBookmarkletCommand(),
-		});
+    this.addCommand({
+      id: "copy-bookmarklet-address",
+      name: "copy the Obsidian Clipper bookmarklet for this vault",
+      callback: () => this.handleCopyBookmarkletCommand(),
+    });
 
-		this.registerObsidianProtocolHandler("obsidian-clipper", async (e) => {
-			const parameters = e as unknown as Parameters;
+    this.registerObsidianProtocolHandler("obsidian-clipper", async (e) => {
+      const parameters = e as unknown as Parameters;
 
-			let url = parameters.url;
-			let title = parameters.title;
-			let highlightData = parameters.highlightdata;
+      let url = parameters.url;
+      let title = parameters.title;
+      let highlightData = parameters.highlightdata;
 
-			let noteEntry = new ClippedNoteEntry(
-				title,
-				url,
-				this.settings,
-				this.app,
-				highlightData
-			);
+      let noteEntry = new ClippedNoteEntry(
+        title,
+        url,
+        this.settings,
+        this.app,
+        highlightData
+      );
 
-			if (this.settings.useDailyNote) {
-				new DailyPeriodicNoteEntry(
-					this.app,
-					this.settings.openFileOnWrite,
-					this.settings.dailyPosition
-				).writeToPeriodicNote(
-					noteEntry,
-					this.settings.dailyNoteHeading
-				);
-			}
+      if (this.settings.useDailyNote) {
+        new DailyPeriodicNoteEntry(
+          this.app,
+          this.settings.openFileOnWrite,
+          this.settings.dailyPosition
+        ).writeToPeriodicNote(
+          noteEntry,
+          this.settings.dailyNoteHeading
+        );
+      }
 
-			if (this.settings.useWeeklyNote) {
-				new WeeklyPeriodicNoteEntry(
-					this.app,
-					this.settings.openFileOnWrite,
-					this.settings.weeklyPosition
-				).writeToPeriodicNote(
-					noteEntry,
-					this.settings.weeklyNoteHeading
-				);
-			}
-		});
-	}
+      if (this.settings.useWeeklyNote) {
+        new WeeklyPeriodicNoteEntry(
+          this.app,
+          this.settings.openFileOnWrite,
+          this.settings.weeklyPosition
+        ).writeToPeriodicNote(
+          noteEntry,
+          this.settings.weeklyNoteHeading
+        );
+      }
+    });
+  }
 
-	onunload() {}
+  onunload() { }
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+  async loadSettings() {
+    this.settings = Object.assign(
+      {},
+      DEFAULT_SETTINGS,
+      await this.loadData()
+    );
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-	handleCopyBookmarkletCommand() {
-		navigator.clipboard.writeText(
-			new BookmarketlGenerator(
-				this.app.vault.getName()
-			).generateBookmarklet()
-		);
-		new Notice("Obsidian Clipper Bookmarklet copied to clipboard.");
-	}
+  handleCopyBookmarkletCommand() {
+    navigator.clipboard.writeText(
+      new BookmarketlGenerator(
+        this.app.vault.getName()
+      ).generateBookmarklet()
+    );
+    new Notice("Obsidian Clipper Bookmarklet copied to clipboard.");
+  }
 }
 
 class SettingTab extends PluginSettingTab {
-	plugin: ObsidianClipperPlugin;
-	private view: SvelteComponent;
+  plugin: ObsidianClipperPlugin;
+  private view: SvelteComponent;
 
-	constructor(app: App, plugin: ObsidianClipperPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: ObsidianClipperPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const { containerEl } = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		this.view = new SettingsComponent({
-			target: containerEl,
-			props: {
-				settings: writable(this.plugin.settings),
-				app: this.app,
-			},
-		});
-	}
+    const { subscribe, set, update } = writable(this.plugin.settings);
+    let _this = this;
 
-	async hide() {
-		super.hide();
-		await this.plugin.saveSettings();
-		this.view.$destroy();
-	}
+    this.view = new SettingsComponent({
+      target: containerEl,
+      props: {
+        settings: {
+          subscribe,
+          update,
+          // save the plugin values when setting the store
+          set: (value: ObsidianClipperSettings) => {
+            set(value);
+            _this.plugin.saveSettings();
+          },
+        },
+        app: this.app,
+      },
+    });
+  }
+
+  async hide() {
+    super.hide();
+    this.view.$destroy();
+  }
 }
