ab3602543a597184520618595d8e96123d459659
John Christopher <jgchristopher@gmail.com>
2023-02-16 20:20:07 -0600

    Break out parts into components

---
+++ src/settings/CommonSettingsGroup.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { settings } from "./settingsstore";
+</script>
+
+<div class="clp_section_margin">
+	<h1>Common Settings</h1>
+	<div class="setting-item">
+		<div class="setting-item-info">
+			<div class="setting-item-name">Tags</div>
+			<div class="setting-item-description">
+				What tags would you like added to the captured highlights?
+			</div>
+		</div>
+		<div class="setting-item-control">
+			<input
+				type="text"
+				bind:value={$settings.tags}
+				spellcheck="false"
+				placeholder="tags,seperated,by,commas"
+			/>
+		</div>
+	</div>
+	<div class="setting-item">
+		<div class="setting-item-info">
+			<div class="setting-item-name">Date Time Format</div>
+			<div class="setting-item-description">
+				<div>
+					Format you would like to use for the &#123&#123 time
+					&#125&#125 template in clippings. See
+				</div>
+				<a href="https://momentjs.com/docs/#/displaying/format/"
+					>format reference</a
+				>
+			</div>
+		</div>
+		<div class="setting-item-control">
+			<input
+				type="text"
+				bind:value={$settings.timestampFormat}
+				spellcheck="false"
+				placeholder="HH:mm"
+			/>
+		</div>
+	</div>
+	<div class="setting-item">
+		<div class="setting-item-info">
+			<div class="setting-item-name">Clipped Entry Template</div>
+			<div class="setting-item-description">
+				Choose the file to use as a template for the clipped entry
+			</div>
+		</div>
+		<div class="setting-item-control">
+			<input
+				type="text"
+				bind:value={$settings.dailyEntryTemplateLocation}
+				spellcheck="false"
+			/>
+		</div>
+	</div>
+</div>
---
+++ src/settings/DailySettingsGroup.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { slide } from "svelte/transition";
+	import { settings } from "./settingsstore";
+</script>
+
+<div class="clp_section_margin">
+	<div class="setting-item mod-toggle">
+		<div class="setting-item-info">
+			<h1 class="setting-item-name">Daily Note Entry</h1>
+		</div>
+		<div class="setting-item-control">
+			<label
+				class="checkbox-container"
+				class:is-enabled={$settings.useDailyNote}
+			>
+				<input type="checkbox" bind:checked={$settings.useDailyNote} />
+			</label>
+		</div>
+	</div>
+	{#if $settings.useDailyNote}
+		<div
+			in:slide|local={{ duration: 300 }}
+			out:slide|local={{ duration: 300 }}
+		>
+			<div class="setting-item">
+				<div class="setting-item-info">
+					<div class="setting-item-name">Daily Note Header</div>
+					<div class="setting-item-description">
+						What header should highlight data be prepended under in
+						your daily note?
+					</div>
+				</div>
+				<div class="setting-item-control">
+					<input
+						type="text"
+						bind:value={$settings.dailyNoteHeading}
+						spellcheck="false"
+						placeholder=""
+					/>
+				</div>
+			</div>
+			<div class="setting-item">
+				<div class="setting-item-info">
+					<div class="setting-item-name">Daily Note Position</div>
+					<div class="setting-item-description">
+						Would you like to prepend clippings to the top of the
+						section or append them to the bottom of the section?
+					</div>
+				</div>
+				<div class="setting-item-control">
+					<select
+						class="dropdown"
+						bind:value={$settings.dailyPosition}
+					>
+						<option value="prepend">prepend</option>
+						<option value="append">append</option>
+					</select>
+				</div>
+			</div>
+		</div>
+	{/if}
+</div>
