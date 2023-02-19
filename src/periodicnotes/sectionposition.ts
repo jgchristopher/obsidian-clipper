729e2d8005c4159d0fac4e43d89f449fd4037c72
John Christopher <jgchristopher@gmail.com>
2023-02-18 21:18:53 -0600

    +pr 10 address comment about sectionposition

--- src/periodicnotes/sectionposition.ts
+++ src/periodicnotes/sectionposition.ts
@@ -1,6 +1,7 @@
 export const SectionPosition = {
-	PREPEND: "prepend",
-	APPEND: "append",
-};
+  PREPEND: "prepend",
+  APPEND: "append",
+} as const;
+
 export type SectionPosition =
-	typeof SectionPosition[keyof typeof SectionPosition];
+  typeof SectionPosition[keyof typeof SectionPosition];
