522c1dde83c19808ada34979124b156874ec26c4
John Christopher <jgchristopher@gmail.com>
2023-05-04 18:30:27 -0600

    remove commented code

--- src/settings/sveltesettingstypes.ts
+++ src/settings/sveltesettingstypes.ts
@@ -3,6 +3,6 @@ import type { SvelteComponent } from 'svelte';
 export type TabItem = {
 	label: string;
 	value: number;
-	component: typeof SvelteComponent; // new (...args: unknown[]) => SvelteComponent;
+	component: typeof SvelteComponent;
 	props?: Record<string, unknown>;
 };
