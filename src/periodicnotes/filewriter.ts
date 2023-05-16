11253b2e7f89f7fa9e009c3502bedac08b0fc539
John Christopher <jgchristopher@gmail.com>
2023-05-16 14:25:08 -0500

    Frontmatter bug (#31)
    * fix bug with finding frontmatter ending
    
    * remove debugger and bump version

--- src/periodicnotes/filewriter.ts
+++ src/periodicnotes/filewriter.ts
@@ -172,7 +172,7 @@ export abstract class FileWriter {
 			if (cache) {
 				const sections = cache.getFileCache(file)?.sections;
 				const frontmatter = sections?.find((item: SectionCache) => {
-					return (item.type = 'yaml');
+					return item.type === 'yaml';
 				});
 				if (frontmatter) {
 					endLine = frontmatter.position.end.line;
