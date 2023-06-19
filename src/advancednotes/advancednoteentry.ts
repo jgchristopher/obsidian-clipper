1d10d392d319d450f2c7ff8bbb37fda37aad75a0
John Christopher <jgchristopher@gmail.com>
2023-06-18 21:34:42 -0500

    create storage folder location if it doesn't exist

--- src/advancednotes/advancednoteentry.ts
+++ src/advancednotes/advancednoteentry.ts
@@ -1,4 +1,4 @@
-import { TFile, type App } from 'obsidian';
+import { Notice, TFile, TFolder, type App } from 'obsidian';
 import { NoteEntry } from 'src/abstracts/noteentry';
 import { AppendWriter } from 'src/periodicnotes/appendwriter';
 import { SectionPosition } from 'src/settings/types';
@@ -17,20 +17,33 @@ export class AdvancedNoteEntry extends NoteEntry {
 		url: string
 	) {
 		const noteFilePath = `${this.storageFolder}/${hostName}.md`;
-		const file = this.app.vault.getAbstractFileByPath(noteFilePath);
+		const folder = this.app.vault.getAbstractFileByPath(this.storageFolder);
+		let file = this.app.vault.getAbstractFileByPath(noteFilePath);
 
 		const sectionHeader = window.moment().toISOString().replaceAll(':', '-');
 		const entry = `\n# ${sectionHeader} \n ${data}\n[^1] \n\n [^1]: ${url}  \n`;
 
 		if (!(file instanceof TFile)) {
 			// create the file and write data
-			this.app.vault.create(noteFilePath, entry);
+			if (!(folder instanceof TFolder)) {
+				this.app.vault.createFolder(this.storageFolder);
+				await new Promise((r) => setTimeout(r, 50));
+			}
+			file = await this.app.vault.create(noteFilePath, entry);
 		} else {
 			new AppendWriter(this.app, this.openFileOnWrite).write(file, entry);
 		}
 		// Wait for the new note or note data to be available then return
 		await new Promise((r) => setTimeout(r, 50));
 
-		return `![[${hostName}#${sectionHeader}]]`;
+		if (!file) {
+			const errorMessage = `Unable to create clipper storage file. Most likely ${this.storageFolder} doesn't exist and we were unable to create it.`;
+
+			console.error(errorMessage);
+			new Notice(errorMessage);
+			throw Error(errorMessage);
+		}
+
+		return `![[${this.storageFolder}/${hostName}#${sectionHeader}]]`;
 	}
 }
