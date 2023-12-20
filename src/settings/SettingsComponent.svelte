0a354ac457286ce1aaf6ae2b55100ca77a41f6f1
John Christopher <john@jdotc.xyz>
2023-12-19 20:29:29 -0600

    use icons already being used in obsidian for edit and trash

--- src/settings/SettingsComponent.svelte
+++ src/settings/SettingsComponent.svelte
@@ -101,43 +101,55 @@
 								</td>
 							{/if}
 							<td class="text-right">
-								<button
-									on:keypress={() => handleClick(clipper.clipperId)}
-									on:click={() => handleClick(clipper.clipperId)}
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										x="0px"
-										y="0px"
-										width="24"
-										height="24"
-										viewBox="0 0 24 24"
+								<div class="setting-item-control">
+									<span
+										class="clickable-icon setting-editor-extra-setting-button"
+										on:keypress={() => handleClick(clipper.clipperId)}
+										on:click={() => handleClick(clipper.clipperId)}
 									>
-										<path
-											class="clp_action_buttons"
-											d="M 18.414062 2 C 18.158062 2 17.902031 2.0979687 17.707031 2.2929688 L 15.707031 4.2929688 L 14.292969 5.7070312 L 3 17 L 3 21 L 7 21 L 21.707031 6.2929688 C 22.098031 5.9019687 22.098031 5.2689063 21.707031 4.8789062 L 19.121094 2.2929688 C 18.926094 2.0979687 18.670063 2 18.414062 2 z M 18.414062 4.4140625 L 19.585938 5.5859375 L 18.292969 6.8789062 L 17.121094 5.7070312 L 18.414062 4.4140625 z M 15.707031 7.1210938 L 16.878906 8.2929688 L 6.171875 19 L 5 19 L 5 17.828125 L 15.707031 7.1210938 z"
-										></path>
-									</svg>
-								</button>
-								{#if $pluginSettings.clippers.length > 1}
-									<button
-										on:keypress={() => handleDelete(clipper.clipperId)}
-										on:click={() => handleDelete(clipper.clipperId)}
-										><svg
+										<svg
 											xmlns="http://www.w3.org/2000/svg"
-											x="0px"
-											y="0px"
 											width="24"
 											height="24"
 											viewBox="0 0 24 24"
+											fill="none"
+											stroke="currentColor"
+											stroke-width="2"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											class="svg-icon lucide-settings"
+											><path
+												d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"
+											></path><circle cx="12" cy="12" r="3"></circle></svg
 										>
-											<path
-												class="clp_action_buttons"
-												d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z"
-											></path>
-										</svg>
-									</button>
-								{/if}
+									</span>
+									{#if $pluginSettings.clippers.length > 1}
+										<span
+											class="clickable-icon setting-editor-extra-setting-button"
+											on:keypress={() => handleDelete(clipper.clipperId)}
+											on:click={() => handleDelete(clipper.clipperId)}
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												width="24"
+												height="24"
+												viewBox="0 0 24 24"
+												fill="none"
+												stroke="currentColor"
+												stroke-width="2"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												class="svg-icon lucide-trash-2"
+												><path d="M3 6h18"></path><path
+													d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"
+												></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"
+												></path><line x1="10" y1="11" x2="10" y2="17"
+												></line><line x1="14" y1="11" x2="14" y2="17"
+												></line></svg
+											>
+										</span>
+									{/if}
+								</div>
 							</td>
 						</tr>
 					{/each}
